cf32f3f234ecd554365f515218c9f66a8992c6a2
Sergey Sokolov <ssokolov2@alfabank.ru>
2020-04-27 14:18:20 +0300

    test(*): added chmods

--- templates/dockerfile.template.js
+++ templates/dockerfile.template.js
@@ -8,5 +8,7 @@ ARG NGINX_CONF_LOCATION
 WORKDIR /src
 ADD $START_SH_LOCATION /src/start.sh
 ADD $NGINX_CONF_LOCATION /etc/nginx/conf.d/default.conf
+RUN mkdir -p /var/tmp/nginx && chmod 666 /var/tmp/nginx
+RUN mkdir -p /var/lib/nginx && chmod 666 /var/lib/nginx
 ADD . /src
 `;
